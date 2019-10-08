c0566fe8f002526e97500c34cb0a65875a46c33b
dongwei <568211643@qq.com>
2019-10-08 10:24:52 +0800

    圈复杂度脚本2.0 界面化 修复一些 部分函数对应文件名 解析失败 欠缺：可能还存在 部分函数对应文件名 解析失败
    静态不符合项-批量生产版3.0 新增 界面
    目前一个子线程，增加线程池会导致界面卡顿
    改变原本格式，代码创建表格，编辑，保存
    对表格进行设置 字体 大小 样式
    欠缺：1.对不符合 LDRA Testbed Code Review Report.mht 类型文件进行判断
    
    单元测试用例集
    2019/9/16 完成
    2019/9/17 修改封面错误
    
    集成测试用例集
    2019/9/17 完成
    
    单元测试执行情况统计
    2019/9/18完成
    
    2019/9/21
    修复 单元执行情况解析失败漏洞
    修复 单元、集成序列标识解析失败
    
    2019/9/26
    新增函数调用关系
    修复函数解析不全bug
    
    2019/10/8
    对上一版本改动进行补充
    
    修复 静态不符合项文件名正则匹配失败问题 优化正则
    增加 静态度量导出全部圈复杂度大于20的函数信息

--- 静态不符合项列表脚本/Show.py
+++ 静态不符合项列表脚本/Show.py
@@ -184,7 +184,7 @@ class Show:
         for root, dirs, files in os.walk(dirpath):
             #print(root) #当前目录路径
             #print(dirs) #当前路径下所有子目录
-            #print(files) #当前路径下所有非目录子文件
+            #print(files) #当前路径下所有非目录子文件.
             for file in files:
                 if os.path.splitext(file)[1] == '.mht':
                     filelist.append(os.path.join(root, file))
